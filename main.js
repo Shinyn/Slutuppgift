9bfe54534b15b48535309b475bb5f0f856d52786
Shinyn <mattias.alm@hotmail.com>
2022-11-24 05:28:14 +0100

    Added more images, added links to event-list page, styled skapa-event page, added some js

--- main.js
+++ main.js
@@ -2,13 +2,20 @@
 
 /*
 TODO: 
-Lägg till alt på alla bilder
 Använd: $("#nav-placeholder").load("nav.html"); för att ladda in navbar på alla sidor
 */
 
 $(document).ready(function () {
   $(".event-info-button").click(function () {
     $(".event-description").toggle();
+  });
+
+  $(".submit-button").click((event) => {
+    event.preventDefault();
+    $(".form").hide();
+    alert(
+      "Thank you for messaging us, we will get back to you as soon as we can!"
+    );
   });
 });
 
