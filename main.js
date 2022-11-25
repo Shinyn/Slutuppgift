024e8cc70e40b3cf1b14eeed843b8da293bf081f
Shinyn <mattias.alm@hotmail.com>
2022-11-25 01:59:45 +0100

    Lots of styling, submit button on om-oss page now works as intended

--- main.js
+++ main.js
@@ -7,28 +7,16 @@ Använd: $("#nav-placeholder").load("nav.html"); för att ladda in navbar på al
 
 $(document).ready(function () {
   $(".event-info-button").click(function (className) {
-    const product = $(this).parent();
-    console.log(product);
-    const description = $(".event-description", product);
-    console.log(description.text());
-
     $(this).siblings(".event-description").toggle(this);
-    // $(".event-description").toggle(this);
   });
 
-  $(".submit-button").click((event) => {
+  $(".form").on("submit", function (event) {
     event.preventDefault();
-    $(".form").hide();
-    alert(
-      "Thank you for messaging us, we will get back to you as soon as we can!"
+    $(".form").children().hide();
+    $(".form").append(
+      "<div class='appended'>Thank you for messaging us, we will get back to you as soon as we can!</div>"
     );
   });
+
+  // $("").toggle(this);
 });
-
-const btn = document.getElementsById("test");
-
-btn.addEventLisener("click", function () {
-  console.log("hello");
-});
-
-// form.style.display = "none";
